1803f63573b8f47d7dcf3a583cb59ad8b4cbdc28
Aleksandr <grandmaliks@gmail.com>
2026-04-23 16:05:53 +0400

    Sanctum auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
     protected static function boot()
     {
         parent::boot();
